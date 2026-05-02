af101eed02f6ed62cb0b5ce3bbe93f8935994f48
Jurrien de Klerk <jurrien.de.klerk@gmail.com>
2026-05-02 10:20:16 +0200

    Add Spring Security, Keycloak realm config, and dev user provisioning

---
+++ pallas_server/StoryService/src/main/java/com/pallas/storyservice/config/OpenApiConfig.java
@@ -0,0 +1,53 @@
+package com.pallas.storyservice.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.OAuthFlow;
+import io.swagger.v3.oas.models.security.OAuthFlows;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+  @Value("${keycloak.auth-server-url}/realms/${keycloak.realm}")
+  private String issuerUri;
+
+  @Bean
+  public OpenAPI openAPI() {
+    final String authUrl = issuerUri + "/protocol/openid-connect";
+    return new OpenAPI()
+        .components(
+            new Components()
+                .addSecuritySchemes(
+                    "oauth2",
+                    new SecurityScheme()
+                        .type(SecurityScheme.Type.OAUTH2)
+                        .flows(
+                            new OAuthFlows()
+                                .authorizationCode(
+                                    new OAuthFlow()
+                                        .authorizationUrl(authUrl + "/auth")
+                                        .tokenUrl(authUrl + "/token")))));
+  }
+
+  @Bean
+  public OpenApiCustomizer securityRequirementCustomizer() {
+    return openApi ->
+        openApi
+            .getPaths()
+            .values()
+            .forEach(
+                pathItem ->
+                    pathItem
+                        .readOperations()
+                        .forEach(
+                            operation ->
+                                operation.addSecurityItem(
+                                    new SecurityRequirement().addList("oauth2"))));
+  }
+}
