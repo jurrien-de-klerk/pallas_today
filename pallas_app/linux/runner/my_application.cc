3f273b007c908d40c279088228ba9d33465770dd
Jurrien de Klerk <jurrien.de.klerk@gmail.com>
2026-05-16 12:15:03 +0200

    feat: set window icon on Linux from bundled assets

--- pallas_app/linux/runner/my_application.cc
+++ pallas_app/linux/runner/my_application.cc
@@ -54,6 +54,16 @@ static void my_application_activate(GApplication* application) {
 
   gtk_window_set_default_size(window, 1280, 720);
 
+  // Set window icon from bundled Flutter assets.
+  gchar* exe_path = g_file_read_link("/proc/self/exe", nullptr);
+  gchar* exe_dir = g_path_get_dirname(exe_path);
+  gchar* icon_path = g_build_filename(
+      exe_dir, "data", "flutter_assets", "assets", "pallas_icon.png", nullptr);
+  gtk_window_set_icon_from_file(window, icon_path, nullptr);
+  g_free(icon_path);
+  g_free(exe_dir);
+  g_free(exe_path);
+
   g_autoptr(FlDartProject) project = fl_dart_project_new();
   fl_dart_project_set_dart_entrypoint_arguments(
       project, self->dart_entrypoint_arguments);
