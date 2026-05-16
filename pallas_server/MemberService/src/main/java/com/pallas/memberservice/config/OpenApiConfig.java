c6b1c40dca3e7ac74bca8e4b3db534b4dd4d3ac2
Jurrien de Klerk <jurrien.de.klerk@gmail.com>
2026-05-16 21:18:15 +0200

    Add MemberService Spring Boot scaffold and compose entry

---
+++ pallas_server/MemberService/src/main/java/com/pallas/memberservice/config/OpenApiConfig.java
@@ -0,0 +1,57 @@
+package com.pallas.memberservice.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.OAuthFlow;
+import io.swagger.v3.oas.models.security.OAuthFlows;
+import io.swagger.v3.oas.models.security.Scopes;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class OpenApiConfig {
+
+  @Value("${keycloak.auth-server-url}/realms/${keycloak.realm}")
+  private String issuerUri;
+
+  @Bean
+  public OpenAPI openAPI() {
+    final String authUrl = issuerUri + "/protocol/openid-connect";
+    return new OpenAPI()
+        .components(
+            new Components()
+                .addSecuritySchemes(
+                    "oauth2",
+                    new SecurityScheme()
+                        .type(SecurityScheme.Type.OAUTH2)
+                        .flows(
+                            new OAuthFlows()
+                                .authorizationCode(
+                                    new OAuthFlow()
+                                        .authorizationUrl(authUrl + "/auth")
+                                        .tokenUrl(authUrl + "/token")
+                                        .scopes(new Scopes())))));
+  }
+
+  @Bean
+  public OpenApiCustomizer securityRequirementCustomizer() {
+    return openApi ->
+        openApi
+            .getPaths()
+            .values()
+            .forEach(
+                pathItem ->
+                    pathItem
+                        .readOperations()
+                        .forEach(
+                            op ->
+                                op.addSecurityItem(
+                                    new SecurityRequirement().addList("oauth2"))));
+  }
+}
---
+++ pallas_server/MemberService/src/main/java/com/pallas/memberservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.pallas.memberservice.exception;
+
+import com.pallas.logger.PallasBacktrace;
+import com.pallas.memberservice.model.Error;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+@Log4j2
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(ResponseStatusException.class)
+  public ResponseEntity<Error> handleResponseStatusException(ResponseStatusException ex) {
+    log.warn("Request failed with status {}", ex.getStatusCode());
+    Error error = new Error();
+    error.setMessage(ex.getReason() != null ? ex.getReason() : ex.getMessage());
+    error.setCode(ex.getStatusCode().toString());
+    return ResponseEntity.status(ex.getStatusCode()).body(error);
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Error> handleValidationException(MethodArgumentNotValidException ex) {
+    log.warn("Request validation failed: {} field error(s)", ex.getBindingResult().getErrorCount());
+    Error error = new Error();
+    String message =
+        ex.getBindingResult().getFieldErrors().stream()
+            .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
+            .findFirst()
+            .orElse("Validation failed");
+    error.setMessage(message);
+    error.setCode(HttpStatus.BAD_REQUEST.toString());
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<Error> handleGenericException(Exception ex) {
+    log.error("Unhandled exception of type {}", ex.getClass().getName());
+    PallasBacktrace.backtrace(log);
+    Error error = new Error();
+    error.setMessage("Internal server error");
+    error.setCode(HttpStatus.INTERNAL_SERVER_ERROR.toString());
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+  }
+}
