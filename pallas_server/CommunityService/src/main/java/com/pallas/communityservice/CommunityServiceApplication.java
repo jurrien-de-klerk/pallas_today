3982d64dd7afcfbba013e9cf16aeac06a2216ef2
Jurrien de Klerk <jurrien.de.klerk@gmail.com>
2026-06-13 11:31:29 +0200

    Add @ComponentScan to enable pallas-integrations bean discovery

--- pallas_server/CommunityService/src/main/java/com/pallas/communityservice/CommunityServiceApplication.java
+++ pallas_server/CommunityService/src/main/java/com/pallas/communityservice/CommunityServiceApplication.java
@@ -2,8 +2,10 @@ package com.pallas.communityservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.pallas.communityservice", "com.pallas.integrations"})
 @SuppressWarnings("PMD.UseUtilityClass")
 public class CommunityServiceApplication {
   public static void main(String[] args) {
--- pallas_server/StoryService/src/main/java/com/pallas/storyservice/StoryServiceApplication.java
+++ pallas_server/StoryService/src/main/java/com/pallas/storyservice/StoryServiceApplication.java
@@ -2,8 +2,10 @@ package com.pallas.storyservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.pallas.storyservice", "com.pallas.integrations"})
 @SuppressWarnings("PMD.UseUtilityClass")
 public class StoryServiceApplication {
   public static void main(String[] args) {
