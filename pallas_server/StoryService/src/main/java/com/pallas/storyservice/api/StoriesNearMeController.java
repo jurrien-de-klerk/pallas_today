5446f8c102f372e193fd4811a08d44c75da371d1
Jurrien de Klerk <jurrien.de.klerk@gmail.com>
2026-06-24 00:46:53 +0200

    feat: add endpoint to retrieve personal Stories near you feed with pagination

--- pallas_server/StoryService/src/main/java/com/pallas/storyservice/api/StoriesNearMeController.java
+++ pallas_server/StoryService/src/main/java/com/pallas/storyservice/api/StoriesNearMeController.java
@@ -1,8 +1,6 @@
 package com.pallas.storyservice.api;
 
 import com.pallas.storyservice.model.StoriesPage;
-import java.time.OffsetDateTime;
-import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +8,10 @@ import org.springframework.web.server.ResponseStatusException;
 
 /** REST controller implementing the generated {@link StoriesNearYouApi} interface. */
 @RestController
-public class StoriesNearYouController implements StoriesNearYouApi {
+public class StoriesNearMeController implements StoriesNearMeApi {
 
   @Override
-  public ResponseEntity<StoriesPage> getStoriesNearYou(
-      UUID memberId, Integer limit, OffsetDateTime before) {
+  public ResponseEntity<StoriesPage> getStoriesNearMe(Integer limit, Integer before) {
     throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED, "Endpoint not yet implemented");
   }
 }
